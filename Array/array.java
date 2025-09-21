8963e5ac65c3ff5748be025d63bd3745f1e43a62
XCELBOTv2 <xcelbot123@gmail.com>
2025-09-22 01:11:52 +0800

    array.java
    A Java program that stores student names and grades in arrays. It provides a menu to display all students, search by name, show the highest grade, and compute the average grade. The menu repeats until the user chooses to exit.

--- Array/array.java
+++ Array/array.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class arraylists {
+public class array {
     public static void main(String[] args) {
         int c;
         
@@ -85,4 +85,5 @@ public class arraylists {
             }
             
     }
+
 
