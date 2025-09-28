1e4d82fc36f729f5015bb22a0594438f6fffd2da
XCELBOTv2 <xcelbot123@gmail.com>
2025-09-29 01:13:28 +0800

    Update Sort.java

--- Array/Sort.java
+++ Array/Sort.java
@@ -1,21 +1,7 @@
 import java.util.ArrayList;
-public class arraylist {
+public class Sort {
     public static void main(String[] args) {
-        /*ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(25);
-        numbers.add(45);
-        numbers.add(65);
-        for(int i = numbers.size()-1; i>=0; i--){
-            System.out.println("The numbers are "+numbers.get(i));
-        }
-        ArrayList<arraylist2> numbers = new ArrayList<arraylist2>();
-        numbers.add(new arraylist2(45));
-        numbers.add(new arraylist2(25));
-        numbers.add(new arraylist2(15));
-
-        for (arraylist2 n : numbers){
-            n.getnumber();
-        }*/
+       
         int[] numbers ={23,34,17,89,90,65,24,12,8,79};
         
         for(int number : numbers){
@@ -39,3 +25,4 @@ public class arraylist {
     }
 
       
+
